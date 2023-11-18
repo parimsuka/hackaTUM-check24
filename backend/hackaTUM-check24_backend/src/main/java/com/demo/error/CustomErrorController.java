aa582e4e6bc6d7c0d7cd5fcd184c58dd12237bbd
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2023-11-18 13:07:17 +0200

    submit button callback function request added

---
+++ backend/hackaTUM-check24_backend/src/main/java/com/demo/error/CustomErrorController.java
@@ -0,0 +1,24 @@
+package com.demo.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+
+    @RequestMapping("/error")
+    @ResponseBody
+    String error(HttpServletRequest request) {
+        return "<h1>Error occurred</h1>";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
