1536202dcff148cc04fb9ea1a717736b333c075f
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2023-11-18 17:27:00 +0200

    markers added to the map

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -4,9 +4,23 @@ import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 export default function ShowExpertsMap(){
+    const [markers, setMarkers] = useState([
+        {
+            id:"0",
+            name:"garching",
+            lng:11.6534301,
+            lat:48.2525859
+        },
+        {
+            id:"1",
+            name:"Ramersdorf-Perlach",
+            lng:11.6138229,
+            lat:48.0974410
+        }
+    ]);
     const mapContainer = useRef<HTMLDivElement |null>(null);
-    const map = useRef<maptilersdk.Map | null>(null);
-    const tokyo = { lng: 139.753, lat: 35.6844 };
+    const map = useRef<maptilersdk.Map|null>(null);
+    const munich = { lng: 11.576124, lat: 48.137154};
     const [zoom] = useState(14);
     maptilersdk.config.apiKey = `${process.env.NEXT_PUBLIC_MAPS_API_KEY}`;
     useEffect(() => {
@@ -15,11 +29,16 @@ export default function ShowExpertsMap(){
         map.current = new maptilersdk.Map({
             container: mapContainer.current ?? '',
             style: maptilersdk.MapStyle.STREETS,
-            center: [tokyo.lng, tokyo.lat],
+            center: [munich.lng, munich.lat],
             zoom: zoom
         });
+        markers.map((value,index) => {
+            new maptilersdk.Marker({color: "#013474"})
+                .setLngLat([value.lng,value.lat])
+                .addTo(map.current as maptilersdk.Map);
+        })
 
-    }, [tokyo.lng, tokyo.lat, zoom]);
+    }, [munich.lng, munich.lat, zoom]);
     return(
             <div className="bg-view-main h-full w-full">
                 <div className="w-full h-full">
