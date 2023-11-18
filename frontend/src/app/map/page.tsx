52e51c26498b19189c31599ec02578d583b8cc9c
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2023-11-19 00:26:04 +0200

    creating custom markers and evaluating their size depending on the rank

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -4,21 +4,28 @@ import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import Navbar from "@/components/NavBar";
+import "@/../marker.css"
+import {object} from "prop-types";
 export default function ShowExpertsMap(){
     const [markers, setMarkers] = useState([
         {
             id:"0",
             name:"garching",
+            ranking:5,
+            description:"This is a very good place in garching",
             lng:11.6534301,
             lat:48.2525859
         },
         {
             id:"1",
             name:"Ramersdorf-Perlach",
+            description:"Best place in germany",
             lng:11.6138229,
+            ranking:7,
             lat:48.0974410
         }
     ]);
+
     const mapContainer = useRef<HTMLDivElement |null>(null);
     const map = useRef<maptilersdk.Map|null>(null);
     const munich = { lng: 11.576124, lat: 48.137154};
@@ -34,11 +41,32 @@ export default function ShowExpertsMap(){
             zoom: zoom
         });
         markers.map((value,index) => {
-            new maptilersdk.Marker({color:"#013474"})
+            let popup = new maptilersdk.Popup({ offset: 30 })
+                .setHTML(`<h1 style="font-weight: bold">${value.name}</h1>  <p> ${value.description} </p>`)
+            // popup.on("open", () =>{
+            //     console.log(5)
+            // })
+            let ma = new maptilersdk.Marker({className:"marker"})
                 .setLngLat([value.lng,value.lat])
-                .setPopup(new maptilersdk.Popup().setHTML("&lt;h1&gt;Hello World!&lt;/h1&gt;"))
-                .addTo(map.current as maptilersdk.Map);
+                .setPopup(popup)
+                .addTo(map.current as maptilersdk.Map)
+                ma.addClassName(value.id)
+            let oldMin = 1
+            let oldMax = 10
+            let newMin = 2
+            let newMax = 5
+            let newValue = (((value.ranking - oldMin)* (newMax - newMin)) / (oldMax - oldMin));
+            let makerElement =  document.getElementsByClassName(`${value.id}`);
+
+            for (let i = 0; i < makerElement.length; i++ ) {
+                console.log(newValue)
+                makerElement[i].setAttribute("style", `width:${newValue}rem;height:${newValue}rem`)
+            }
+
+            ma.getElement().innerHTML = `<span style="z-index: 9999;color: white">${value.ranking}</span>`
+
         })
+
 
     }, [munich.lng, munich.lat, zoom]);
 
