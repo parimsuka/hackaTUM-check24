9693d28278b48726bfdb43fee23b6c31ff302616
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2023-11-19 09:39:14 +0200

    styles and inputs for the form adjusted

--- frontend/src/components/profile-form.tsx
+++ frontend/src/components/profile-form.tsx
@@ -6,21 +6,27 @@ import {useEffect, useState} from "react";
 export default function ProfileForm(props:{x:number,y:number,offsetX:number}){
     const [user,setUser] = useState({
         name:"Omar",
-        last:"Ashour"
+        last:"Ashour",
+        lastone:"helo"
     });
     return(
         ReactDOM.createPortal(
             <div style={{left:props.x+props.offsetX, top:props.y}} className={`bg-view-main absolute z-[999999]`}>
                 <div className="w-[324px] h-[382px] border-[1px] border-[#B4B4B4] rounded-2xl">
-                    <div className="grid gap-5">
-                        <div className="flex space-x-5">
+                    <div className="p-[3rem] grid">
+                        <div className="flex flex-col gap-[3rem]">
+                        <div className="flex">
                             <Image alt={"user"} src={manAvatarSvg}/>
                             <h1>name</h1>
                         </div>
+                        <div className="grid w-full gap-[2rem]">
                         <input onInput={(e) => {setUser({...user, name: (e.target as any).value})}} value={user.name} type={"input"}/>
-                        <input onInput={(e) => {setUser({...user, last: (e.target as any).value})}} value={user.last} type={"input"}/>
+                            <input onInput={(e) => {setUser({...user, lastone: (e.target as any).value})}} value={user.lastone} type={"input"}/>
+                            <input onInput={(e) => {setUser({...user, last: (e.target as any).value})}} value={user.last} type={"input"}/>
                         <input onClick={() => {
                         }} type={"button"} className="bg-button-main text-white" value={"Submit"}/>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div> ,
