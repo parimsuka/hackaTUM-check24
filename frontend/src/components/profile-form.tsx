e87d9c5c5fe4777dbcc8d000ea7d94fa7d2ef85b
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2023-11-19 10:48:22 +0200

    finalize basic form for editing user

--- frontend/src/components/profile-form.tsx
+++ frontend/src/components/profile-form.tsx
@@ -12,8 +12,8 @@ export default function ProfileForm(props:{x:number,y:number,offsetX:number}){
         ReactDOM.createPortal(
             <div style={{left:props.x+props.offsetX, top:props.y}} className={`bg-view-main absolute z-[999999]`}>
                 <div className="w-[324px] h-[382px] border-[1px] border-[#B4B4B4] rounded-2xl">
-                    <div className="p-[3rem] grid">
-                        <div className="flex flex-col gap-[3rem]">
+                    <div className="p-[2rem] grid">
+                        <div className="flex flex-col gap-[1rem]">
                         <div className="flex justify-evenly">
                             <Avatar/>
                             <div>
@@ -22,9 +22,9 @@ export default function ProfileForm(props:{x:number,y:number,offsetX:number}){
                             </div>
                         </div>
                         <div className="grid w-full gap-[2rem]">
-                        <input onInput={(e) => {setUser({...user, name: (e.target as any).value})}} value={user.name} type={"input"}/>
-                            <input onInput={(e) => {setUser({...user, lastone: (e.target as any).value})}} value={user.lastone} type={"input"}/>
-                            <input onInput={(e) => {setUser({...user, last: (e.target as any).value})}} value={user.last} type={"input"}/>
+                            <input className="bg-[#B4B4B4] h-10 rounded-lg" onInput={(e) => {setUser({...user, name: (e.target as any).value})}} value={user.name} type={"input"}/>
+                            <input className="bg-[#B4B4B4] h-10 rounded-lg" onInput={(e) => {setUser({...user, lastone: (e.target as any).value})}} value={user.lastone} type={"input"}/>
+                            <input className="bg-[#B4B4B4] h-10 rounded-lg" onInput={(e) => {setUser({...user, last: (e.target as any).value})}} value={user.last} type={"input"}/>
                         <input onClick={() => {
                         }} type={"button"} className="bg-button-main text-white" value={"Submit"}/>
                         </div>
